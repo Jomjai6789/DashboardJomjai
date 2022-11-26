1984487a87e7628d166c121c849c4fb57296216d
ung <test@abc.com>
2022-11-26 10:30:34 +0700

    Responsive

---
+++ lib/state_website/widget_dashboard_mobile.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class WidgetDashboardMobile extends StatelessWidget {
+  const WidgetDashboardMobile({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Text('Show Website on mobile');
+  }
+}
