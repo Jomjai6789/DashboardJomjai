a9e6bbec6887cec066669441862ec95841d06fdc
ung <test@abc.com>
2022-11-26 10:13:45 +0700

    Check Web

---
+++ lib/state_website/dashboard_website.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/material.dart';
+
+class DashBoardWebsite extends StatelessWidget {
+  const DashBoardWebsite({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+       body: SafeArea(child: Text('This is Website')),
+    );
+  }
+}
